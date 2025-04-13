2148d090a5f4f060455436132a6603432ef814b7
Lightmaker777 <j.kochkorian@gmx.de>
2025-04-13 18:46:31 +0200

    postgres connection for render

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False, index=True)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     role = db.Column(db.String(20), default='mitarbeiter')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
